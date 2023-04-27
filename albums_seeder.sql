d892c6e17ed459e01bc151d7dd43cfd547c63e52
[Czakeria daniels <zakeria.daniels01@gmail.com>
2023-04-27 11:31:41 -0500

    "completed"

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -1,6 +1,8 @@
 USE codeup_test_db;
+TRUNCATE albums;
 
-INSERT INTO albums (artist, name, release_date, sales, genre)
+INSERT INTO albums (
+                    artist, name, release_date, sales, genre)
     VALUE ('Led Zeppelin', 'Led Zeppelin IV', 1971, 30.4, 'Hard Rock'),
     ('Michael Jackson', 'Thriller', 1982, 51.2, 'Pop'),
     ('AC/DC', 'Back in Black', 1980, 30.1, 'Hard Rock'),
